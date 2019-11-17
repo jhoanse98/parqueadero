b03d6482bab78a4d0f741551de560bd526c155c5
jhoanse98 <mansleobenitez@gmail.com>
2019-11-17 16:29:28 -0500

    first advance of the project

--- src/App.js
+++ src/App.js
@@ -1,26 +1,41 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
 
-function App() {
+class App extends React.Component {
+
+  state={
+    posts:[]
+  }
+
+  componentDidMount(){
+    axios.get("https://www.reddit.com/r/aww.json")
+    .then(response => {
+      this.setState({posts:response.data.data.children});
+    })
+    .catch(error=>{
+      console.log(error);
+    })
+  }
+
+  render(){
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ul className="list-group list-group-flush">
+        {this.state.posts.map(post => 
+          <li key={post.data.id} className="list-group-item flex-container">
+            <img src={post.data.thumbnail} alt="thumb" className="thumbnail" />
+            <div>{post.data.title}</div>
+            </li>
+
+          )}
+      </ul>
+      
+      
     </div>
   );
+  }
 }
 
 export default App;
